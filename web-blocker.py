3fa4cb82a1d336d83c7a7c2f2bd4c4e82a5d72a8
Jeremy Collins <jeremy@jeremycollins.me>
2017-08-22 23:01:20 -0500

    Write to the hosts file

--- web-blocker.py
+++ web-blocker.py
@@ -1,13 +1,22 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp = "hosts"
 hosts_path = r"/etc/hosts"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.com"]
 
+# Set the working hours time (e.g. .day,8 and .day,16 for 8am-4pm)
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
     else:
         print("Non-working hours...")
     time.sleep(5)
